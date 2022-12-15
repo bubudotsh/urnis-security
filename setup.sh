b6efe9e7e44d81420c0097066da9d29b9b930ab1
bubudotsh <bunelierpro@gmail.com>
2022-12-15 18:48:20 +0100

    mod all

--- setup.sh
+++ setup.sh
@@ -17,12 +17,12 @@ echo -e "[ Urnis setup ]\n\n"
 
 whe=$(pwd)
 sudo mkdir /usr/share/urnis
-sudo cp -fr .* /usr/share/urnis
-sudo cp -fr src/urnis /usr/sbin/urnis
-sed -i -e "s/aaa/${whe}/g" /usr/sbin/urnis/src/clean.sh
-chmod +x /usr/share/urnis/src/clean.sh
-chmod +x /usr/share/urnis/src/sshlogin.sh
-chmod +x /usr/share/urnis/audit.sh
+sudo cp -fr .* /usr/share/urnis/
+sudo cp -fr src/urnis /usr/sbin/
+sudo sed -i -e "s/aaa/${whe}/g" /usr/sbin/urnis/src/clean.sh
+sudo chmod +x /usr/share/urnis/src/clean.sh
+sudo chmod +x /usr/share/urnis/src/sshlogin.sh
+sudo chmod +x /usr/share/urnis/audit.sh
 
 sudo touch /etc/ssh/sshrc
 sudo echo "bash /usr/share/urnis/src/sshlogin.sh" >> /etc/ssh/sshrc
