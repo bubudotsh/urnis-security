2f3955f17e8cc77c5b6cbb90a9b000561dc36ebd
bubudotsh <bunelierpro@gmail.com>
2022-12-16 17:05:06 +0100

    readme and help

--- setup.sh
+++ setup.sh
@@ -15,36 +15,24 @@ where=$(pwd)
 echo -e "[ Urnis setup ]\n\n"
 
 
+pathd="/usr/share"
+pathf="/usr/sbin/"
 
+if [ -d ${pathd} ]; then
+    sudo mkdir /usr/share/urnis
+    sudo cp -fr .* /usr/share/urnis/
+else
+    echo -e "${pathd} not found"
+    exit
+fi
 
-whe=$(pwd)
-sudo mkdir /usr/share/urnis
-sudo cp -fr .* /usr/share/urnis/
-sudo cp -fr src/urnis /usr/sbin/
-# sudo chmod +x /usr/share/urnis/src/sshlogin.sh
-# sudo chmod +x /usr/share/urnis/audit.sh
-
-
-
-# pathd="/usr/share"
-# pathf="/usr/sbin/"
-
-# echo -e "[ Urnis setup ]\n\n"
-
-# if [ -d ${pathd} ]; then
-#     sudo mkdir ${pathd}/urnis
-#     sudo cp -fr .* ${pathd}/urnis/
-# else
-#     echo -e "${pathd} not found"
-#     exit
-# fi
-
-# if [ -d ${pathf} ]; then
-#     sudo cp -fr src/urnis /usr/sbin/urnis
-# else
-#     echo -e "${pathf} not found"
-#     exit
-# fi
+if [ -d ${pathf} ]; then
+    sudo cp -fr src/urnis /usr/sbin/
+    sudo chmod +x /usr/sbin/urnis
+else
+    echo -e "${pathf} not found"
+    exit
+fi
 
 echo "Done, you can try sudo urnis -h"
 
