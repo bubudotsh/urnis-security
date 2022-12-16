7d7f8878fcd029ecfe4e959f772aa6426f573887
bubudotsh <bunelierpro@gmail.com>
2022-12-16 16:54:12 +0100

    readme and help

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # require netstat
-
 green=$(tput setaf 2)
 red=$(tput setaf 1)
 normal=$(tput sgr0)
@@ -11,15 +10,24 @@ LGREY='\033[0;37m'
 NC='\033[0m'
 
 where=$(pwd)
+pathd="/usr/share"
+pathf="/usr/sbin/"
 
 echo -e "[ Urnis setup ]\n\n"
 
+if [ -d ${pathd} ]; then
+    sudo mkdir ${pathd}/urnis
+    sudo cp .* ${pathd}/urnis/
+else
+    echo -e "${pathd} not found"
+    exit
+fi
 
-whe=$(pwd)
-sudo mkdir /usr/share/urnis
-sudo cp -fr .* /usr/share/urnis/
-sudo cp -fr src/urnis /usr/sbin/
-sudo chmod +x /usr/share/urnis/src/sshlogin.sh
-sudo chmod +x /usr/share/urnis/audit.sh
+if [ -d ${pathf} ]; then
+    sudo cp -fr src/urnis /usr/sbin/
+else
+    echo -e "${pathf} not found"
+    exit
+fi
 
-# sed -i '1s/^/bash \/usr\/share\/urnis\/src\/sshlogin.sh /' ~/.bashrc
+echo "Done, you can try sudo urnis -h"
