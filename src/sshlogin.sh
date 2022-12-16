e2b5087d7da3493d0c1356e038ea08defe803639
bubudotsh <bunelierpro@gmail.com>
2022-12-16 08:29:43 +0100

    mod

--- src/sshlogin.sh
+++ src/sshlogin.sh
@@ -1,5 +1,13 @@
 path="/usr/share/urnis/data/sshlogin"
 
+
+
+# personaliser dir scan
+# fiabilisation (verifier presance dossier...)
+# fix ssh login
+# fix cpu name
+
+
 sender=$(sudo grep mailsender /usr/share/urnis/src/urnis.conf | cut -c 13- | sed 's/"//g')
 passw=$(sudo grep password /usr/share/urnis/src/urnis.conf | cut -c 10- | sed 's/"//g')
 reciver=$(sudo grep mailreciver /usr/share/urnis/src/urnis.conf | cut -c 13- | sed 's/"//g')
