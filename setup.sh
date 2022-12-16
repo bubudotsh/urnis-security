d1b63013369c93e1e7f67ab5934e9c4d747883b9
bubudotsh <bunelierpro@gmail.com>
2022-12-16 17:02:00 +0100

    readme and help

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # require netstat
+
 green=$(tput setaf 2)
 red=$(tput setaf 1)
 normal=$(tput sgr0)
@@ -10,24 +11,41 @@ LGREY='\033[0;37m'
 NC='\033[0m'
 
 where=$(pwd)
-pathd="/usr/share"
-pathf="/usr/sbin/"
 
 echo -e "[ Urnis setup ]\n\n"
 
-if [ -d ${pathd} ]; then
-    sudo mkdir ${pathd}/urnis
-    sudo cp -fr .* ${pathd}/urnis/
-else
-    echo -e "${pathd} not found"
-    exit
-fi
 
-if [ -d ${pathf} ]; then
-    sudo cp -fr src/urnis /usr/sbin/
-else
-    echo -e "${pathf} not found"
-    exit
-fi
+
+
+whe=$(pwd)
+sudo mkdir /usr/share/urnis
+sudo cp -fr .* /usr/share/urnis/
+sudo cp -fr src/urnis /usr/sbin/
+# sudo chmod +x /usr/share/urnis/src/sshlogin.sh
+# sudo chmod +x /usr/share/urnis/audit.sh
+
+
+
+# pathd="/usr/share"
+# pathf="/usr/sbin/"
+
+# echo -e "[ Urnis setup ]\n\n"
+
+# if [ -d ${pathd} ]; then
+#     sudo mkdir ${pathd}/urnis
+#     sudo cp -fr .* ${pathd}/urnis/
+# else
+#     echo -e "${pathd} not found"
+#     exit
+# fi
+
+# if [ -d ${pathf} ]; then
+#     sudo cp -fr src/urnis /usr/sbin/urnis
+# else
+#     echo -e "${pathf} not found"
+#     exit
+# fi
 
 echo "Done, you can try sudo urnis -h"
+
+# sed -i '1s/^/bash \/usr\/share\/urnis\/src\/sshlogin.sh /' ~/.bashrc
